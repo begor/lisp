55a12407ebb9d4f693a02cd367bfe9c03589f8ba
Biryukov Egor <birykovegor@gmail.com>
2016-12-27 20:58:53 +0300

    Parser tests

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,5 +1,17 @@
+import pytest
+
 from lisp.parser import parse
 
-def test_parses():
-    ast = parse('(+ 2 (- 4 (* 2 1)))')
-    assert ast == ['+', 2, ['-', 4, ['*', 2, 1]]]
+
+PROGRAM_2_AST = [
+    ('()', []),
+    ('(+ 2 3.2)', ['+', 2, 3.2]),
+    ('(+ 2 (- 4 (* 2 1)))', ['+', 2, ['-', 4, ['*', 2, 1]]]),
+    ('(+ (+ (+ 1 (+ 2 3)) 4 5 6.2) -7 (- 9 1) 8)',
+     ['+', ['+', ['+', 1, ['+', 2, 3]], 4, 5, 6.2], -7, ['-', 9, 1], 8])
+]
+
+
+@pytest.mark.parametrize('program, ast', PROGRAM_2_AST)
+def test_parses(program, ast):
+    assert parse(program) == ast
